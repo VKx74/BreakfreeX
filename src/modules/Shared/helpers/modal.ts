ab41135625d7cdbca79e51bc3888301557972aab
andriy.yarosh <andriy.yarosh@gmail.com>
2020-09-11 18:45:29 +0300

    lint fix

--- src/modules/Shared/helpers/modal.ts
+++ src/modules/Shared/helpers/modal.ts
@@ -8,7 +8,6 @@ export abstract class Modal<ModalDataType = any, ModalResultType = any> {
     protected constructor(injector: Injector) {
         this.dialogRef = injector.get(MatDialogRef);
         this.data = injector.get(MAT_DIALOG_DATA);
-        this.dialogRef
     }
 
     close(result?: ModalResultType) {
