28d8d47353d3f0db8ff60eecc077b6f2b78351a5
andriy.yarosh <andriy.yarosh@gmail.com>
2020-10-26 14:33:41 +0200

    improvements

--- src/app/services/algo.service.ts
+++ src/app/services/algo.service.ts
@@ -341,7 +341,9 @@ class AlgoServiceEncryptionHelper {
     }
 
     static base64ToHex(str) {
-        for (let i = 0, bin = atob(str.replace(/[ \r\n]+$/, "")), hex = []; i < bin.length; ++i) {
+        let hex = [];
+        let bin = atob(str.replace(/[ \r\n]+$/, ""));
+        for (let i = 0; i < bin.length; ++i) {
             let tmp = bin.charCodeAt(i).toString(16);
             if (tmp.length === 1) tmp = "0" + tmp;
             hex[hex.length] = tmp;
