f7c0d79d883a02b799769c7fee52ab6196494347
andriy.yarosh <andriy.yarosh@gmail.com>
2020-11-09 09:24:39 +0200

    fix error

--- src/modules/Auth/components/credentials-page/credentials-page.component.ts
+++ src/modules/Auth/components/credentials-page/credentials-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {passwordsMatchValidator, passwordValidator} from "Validators";
-import {RegisterUserModel, Roles} from "@app/models/auth/auth.models";
-import {UrlsManager} from "@app/Utils/UrlManager";
-import {AuthRoutes} from "../../auth.routes";
-import {ActivatedRoute, Router} from "@angular/router";
-import {AuthenticationService} from "@app/services/auth/auth.service";
-import {CrossFieldErrorMatcher} from "@app/Utils/crossFieldErrorMatcher";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { passwordsMatchValidator, passwordValidator } from "Validators";
+import { RegisterUserModel, Roles } from "@app/models/auth/auth.models";
+import { UrlsManager } from "@app/Utils/UrlManager";
+import { AuthRoutes } from "../../auth.routes";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthenticationService } from "@app/services/auth/auth.service";
+import { CrossFieldErrorMatcher } from "@app/Utils/crossFieldErrorMatcher";
 
 @Component({
   selector: 'credentials-page',
@@ -20,9 +20,9 @@ export class CredentialsPageComponent implements OnInit {
   errorMatcher = new CrossFieldErrorMatcher();
 
   constructor(private _router: Router,
-              private _route: ActivatedRoute,
-              private _formBuilder: FormBuilder,
-              private _authService: AuthenticationService) {
+    private _route: ActivatedRoute,
+    private _formBuilder: FormBuilder,
+    private _authService: AuthenticationService) {
 
     this.registerFormGroup = this._formBuilder.group({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -51,21 +51,29 @@ export class CredentialsPageComponent implements OnInit {
     this.serverError = null;
 
     this._authService.registerUser(model).subscribe(
-        data => {
-          this.processing = false;
-          this._router.navigate([AuthRoutes.Confirming], {
-            relativeTo: this._route.parent,
-            queryParams: {email: email}
-          });
-        },
-        error => {
-          this.processing = false;
-          this.serverError = 'Registration failed: ' + error.error.description;
+      data => {
+        this.processing = false;
+        this._router.navigate([AuthRoutes.Confirming], {
+          relativeTo: this._route.parent,
+          queryParams: { email: email }
         });
+      },
+      error => {
+        this.processing = false;
+        this.serverError = 'Registration failed: ' + error.error.description;
+      });
   }
 
   doLogin() {
-    this._router.navigate([AuthRoutes.Login], {relativeTo: this._route.parent});
+    this._router.navigate([AuthRoutes.Login], { relativeTo: this._route.parent });
+  }
+
+  doLoginWithGoogle() {
+    window.location.href = this._authService.signInWithGoogleEndpoint;
+  }
+
+  doLoginWithFB() {
+    window.location.href = this._authService.signInWithFBEndpoint;
   }
 
 }
