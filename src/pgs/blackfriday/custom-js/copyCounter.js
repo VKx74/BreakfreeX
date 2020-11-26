7fc2b355c7ebc31f7272074d60dee74a4606cbb3
andriy.yarosh <andriy.yarosh@gmail.com>
2020-11-26 19:31:27 +0200

    removed blackfriday offer

--- src/pgs/blackfriday/custom-js/copyCounter.js
+++
@@ -1,3 +0,0 @@
-$.get("https://system-platform-stage.breakfreetrading.com/v1/sysnotification/api/PromoText", function(data, status) {
-    $(".copy-counter")[0].innerHTML = data + " out of 350 copies remaining.";
-});
