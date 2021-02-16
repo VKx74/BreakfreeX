71ba4a47d630b8de6e3e60e3af1b22f75e229312
andriy.yarosh <andriy.yarosh@gmail.com>
2021-02-16 16:57:35 +0200

    fix lint

--- src/app/services/traking/fb.pixel.tracking.service.ts
+++ src/app/services/traking/fb.pixel.tracking.service.ts
@@ -9,12 +9,12 @@ import { Injectable } from '@angular/core';
             (function (f: any, b, e, v, n, t, s) {
                 if (f.fbq) return; n = f.fbq = function () {
                     n.callMethod ?
-                        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                        n.callMethod.apply(n, arguments) : n.queue.push(arguments);
                 }; if (!f._fbq) f._fbq = n;
                 n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = false;
-                t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
+                t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s);
             })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-            (window as any).fbq.disablePushState = true; //not recommended, but can be done
+            (window as any).fbq.disablePushState = true; // not recommended, but can be done
             (window as any).fbq('init', '2945718008809081');
         }
     }
