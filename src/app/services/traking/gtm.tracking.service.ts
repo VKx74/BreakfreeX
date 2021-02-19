ab9bb7c26f6785167a9e1b573e6b0c93468ee199
andriy.yarosh <andriy.yarosh@gmail.com>
2021-02-19 14:04:38 +0200

    lint fix

--- src/app/services/traking/gtm.tracking.service.ts
+++ src/app/services/traking/gtm.tracking.service.ts
@@ -10,8 +10,8 @@ import { Injectable } from '@angular/core';
                 w[l] = w[l] || []; w[l].push({
                   'gtm.start':
                     new Date().getTime(), event: 'gtm.js'
-                }); var f = d.getElementsByTagName(s)[0],
-                  j = d.createElement(s) as any, dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; 
+                }); let f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s) as any, dl = l !== 'dataLayer' ? '&l=' + l : ''; j.async = true; 
                   j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
               })(window, document, 'script', 'dataLayer', 'GTM-TNPBKC9');
         }
