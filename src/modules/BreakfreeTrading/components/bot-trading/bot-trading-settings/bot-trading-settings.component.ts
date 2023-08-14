a1bb0eb2b6a2bd101012ca55114247988411cd9f
andriy.yarosh <andriy.yarosh@gmail.com>
2023-08-14 18:34:04 +0300

    new neural agent

--- src/modules/BreakfreeTrading/components/bot-trading/bot-trading-settings/bot-trading-settings.component.ts
+++ src/modules/BreakfreeTrading/components/bot-trading/bot-trading-settings/bot-trading-settings.component.ts
@@ -32,7 +32,7 @@ export class BotTradingSettingsComponent extends Modal<BotTradingSettingsCompone
     public profitPolicyAccepted: boolean = false;
     public liabilityWaiverAccepted: boolean = false;
 
-    public downloadLink: string = '/assets/NeuralAgent_1.1.1.ex5';
+    public downloadLink: string = '/assets/NeuralAgent_1.1.2.ex5';
 
     public get isAllowed(): boolean {
         return this._identityService.subscriptionType === SubscriptionType.AI;
