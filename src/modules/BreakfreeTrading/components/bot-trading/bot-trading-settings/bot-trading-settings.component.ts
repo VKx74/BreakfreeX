f04a7110e3b59660014ed0e211eee32a0f170d3f
Asger <asger@breakfreetrading.com>
2023-10-11 19:00:18 +0700

    Show capitulaton, add 1.2 build

--- src/modules/BreakfreeTrading/components/bot-trading/bot-trading-settings/bot-trading-settings.component.ts
+++ src/modules/BreakfreeTrading/components/bot-trading/bot-trading-settings/bot-trading-settings.component.ts
@@ -32,7 +32,7 @@ export class BotTradingSettingsComponent extends Modal<BotTradingSettingsCompone
     public profitPolicyAccepted: boolean = false;
     public liabilityWaiverAccepted: boolean = false;
 
-    public downloadLink: string = '/assets/NeuralAgent_1.1.6.ex5';
+    public downloadLink: string = '/assets/NeuralAgent_1.2.0.ex5';
 
     public get isAllowed(): boolean {
         return this._identityService.isAuthorizedCustomer;
