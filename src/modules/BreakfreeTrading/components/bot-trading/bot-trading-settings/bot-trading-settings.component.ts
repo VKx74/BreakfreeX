c991878e4570c13dbcf70bc2a457279f7f2e544c
andriy.yarosh <andriy.yarosh@gmail.com>
2024-01-23 18:18:14 +0200

    new NA Na documentation

--- src/modules/BreakfreeTrading/components/bot-trading/bot-trading-settings/bot-trading-settings.component.ts
+++ src/modules/BreakfreeTrading/components/bot-trading/bot-trading-settings/bot-trading-settings.component.ts
@@ -39,6 +39,7 @@ export class BotTradingSettingsComponent extends Modal<BotTradingSettingsCompone
 
 
     public downloadLink: string = '/assets/NeuralAgent_AK-2.17.ex5';
+    public downloadUserDocLink: string = '/assets/NA AK-2.17.1 user doc.pdf';
     // public downloadLink2: string = '/assets/NA2_2.0_beta.ex5';
     // public downloadLink3: string = 'https://forms.gle/wSJNCdN1ahYkosmq9';
 
