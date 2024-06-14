16114887c93141e41666c92c66a68b47e1dd126b
andriy.yarosh <andriy.yarosh@gmail.com>
2024-06-14 18:06:26 +0300

    allow chat just for users with subscriptions

--- src/modules/Chat/components/public-chat/public-chat.component.ts
+++ src/modules/Chat/components/public-chat/public-chat.component.ts
@@ -2,6 +2,9 @@ import {Component} from '@angular/core';
 import {ComponentIdentifier} from "@app/models/app-config";
 import {ChatModeToken} from "../../mode.token";
 import {ChatMode} from "../../enums/chat-mode";
+import { MatDialog } from "@angular/material/dialog";
+import { CheckoutComponent } from 'modules/BreakfreeTrading/components/checkout/checkout.component';
+import { IdentityService } from '@app/services/auth/identity.service';
 
 @Component({
     selector: 'public-chat',
@@ -17,7 +20,18 @@ import {ChatMode} from "../../enums/chat-mode";
 export class PublicChatComponent {
     ComponentIdentifier = ComponentIdentifier;
 
+    get hasAccess(): boolean {
+        return this._identityService.isAuthorizedCustomer;
+    }
+
+    constructor(protected _dialog: MatDialog, protected _identityService: IdentityService) {
+    }
+
     ngOnDestroy(): void {
+    }
+
+    processCheckout() {
+        this._dialog.open(CheckoutComponent, { backdropClass: 'backdrop-background' });
     }
 }
 
