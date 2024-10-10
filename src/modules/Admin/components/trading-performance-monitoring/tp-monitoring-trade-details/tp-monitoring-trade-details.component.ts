38503ee17a27dbb52de4b68098ec547c75554fe0
Asger <>
2024-10-10 11:16:16 +0700

    fixed

--- src/modules/Admin/components/trading-performance-monitoring/tp-monitoring-trade-details/tp-monitoring-trade-details.component.ts
+++ src/modules/Admin/components/trading-performance-monitoring/tp-monitoring-trade-details/tp-monitoring-trade-details.component.ts
@@ -15,7 +15,7 @@ import { Modal } from "Shared";
 import { TzUtils } from "TimeZones";
 
 declare let defaultTheme: any;
-declare let fintatechDarkTheme: any;
+declare let BreakfreeDarkTheme: any;
 
 @Component({
     selector: 'tp-monitoring-trade-details',
@@ -141,7 +141,7 @@ export class TPMonitoringTradeDetailsComponent extends Modal<Trade> implements O
     private _getTheme(): any {
         switch (this._themeService.getActiveTheme()) {
             case Theme.Dark:
-                return fintatechDarkTheme;
+                return BreakfreeDarkTheme;
             case Theme.Light:
             default:
                 return defaultTheme;
--- src/modules/Chart/components/tcd/tcd.component.ts
+++ src/modules/Chart/components/tcd/tcd.component.ts
@@ -59,7 +59,7 @@ export interface ITcdComponentState {
 
 declare let defaultTheme: any;
 declare let darkTheme: any;
-declare let fintatechDarkTheme: any;
+declare let BreakfreeDarkTheme: any;
 declare var ResizeObserver;
 
 interface ReplayWaiter {
@@ -920,7 +920,7 @@ export class TcdComponent extends BaseGoldenLayoutItemComponent {
     private _getTheme(): any {
         switch (this._themeService.activeTheme) {
             case Theme.Dark:
-                return fintatechDarkTheme;
+                return BreakfreeDarkTheme;
             case Theme.Light:
             // return fintatech;
             default:
